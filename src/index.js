76a56bf4aa30556a5c1bef318ef92815e9282271
Plumbles <93825786+Plumbles@users.noreply.github.com>
2023-02-25 17:58:02 +0100

    Added necessary files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token} = require('./config.json');
+const fs = require('node:fs');
+
+const client = new Client({
+    intents:[GatewayIntentBits.Guilds]
+})
+
+client.commands = new Collection()
+
+
+//slashCommands
+
+//slashCommands End
