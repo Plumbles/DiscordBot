eb4aa6e9f0d7aa4220f16c0dc2ae0b2a41228930
Plumbles <93825786+Plumbles@users.noreply.github.com>
2023-03-29 17:37:41 +0200

    Updated discord-player to v6.1.1

--- commands/music/move.js
+++ commands/music/move.js
@@ -12,19 +12,19 @@ module.exports = {
             option.setName("to").setDescription("position to move the song").setRequired(true)),
         
     execute: async ({ client, interaction }) => {
-        let fromPos = (await interaction.options.getNumber("from", true));
-        let toPos = (await interaction.options.getNumber("to", true));
+        let fromPos = await interaction.options.getNumber("from", true);
+        let toPos = await interaction.options.getNumber("to", true);
 
         const queue = useQueue(interaction.guildId);
-        const song = queue.tracks.at(fromPos)
+        const song = queue.tracks.at((fromPos - 1));
 
-        queue.moveTrack(fromPos, toPos)
+        queue.moveTrack(song, (toPos - 1))
 
         // Embed
         await interaction.reply({
             embeds: [
                 new EmbedBuilder()
-                .setDescription(`Moved ${queue.tracks[toPos]} to position ${toPos}`)
+                .setDescription(`Moved [${song.title}](${song.url}) to position ${toPos}`)
             ]
         })
 
